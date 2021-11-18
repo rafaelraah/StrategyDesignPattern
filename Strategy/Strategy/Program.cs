600c00de2df89617179435eefe30fa0766c35a84
Rafael <rafaelsantos.rsdn@gmail.com>
2021-11-18 19:36:54 -0300

    First commit

---
+++ Strategy/Strategy/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatterns;
+using System;
+
+namespace Strategy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ExecucaoStrategy.Executar();
+        }
+    }
+}
